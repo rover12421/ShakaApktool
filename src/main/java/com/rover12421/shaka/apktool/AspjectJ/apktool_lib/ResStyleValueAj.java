a01ee789242ad2808a76bb959155aedabed8ac79
Rover12421 <rover12421@163.com>
2014-10-23 17:06:05 +0800

    更新修复
    1.更新Asject到源中当前最新代码
    2.修复新代码中遍历解析dex文件异常退出问题,只要不是主dex文件,解析异常都跳过

--- src/main/java/com/rover12421/shaka/apktool/AspjectJ/apktool_lib/ResStyleValueAj.java
+++ src/main/java/com/rover12421/shaka/apktool/AspjectJ/apktool_lib/ResStyleValueAj.java
@@ -1,5 +1,6 @@
 package com.rover12421.shaka.apktool.AspjectJ.apktool_lib;
 
+import brut.androlib.AndrolibException;
 import brut.androlib.res.data.ResResSpec;
 import brut.androlib.res.data.ResResource;
 import brut.androlib.res.data.value.ResAttr;
@@ -14,6 +15,8 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.xmlpull.v1.XmlSerializer;
 
+import java.io.IOException;
+
 /**
  * Created by rover12421 on 8/9/14.
  */
@@ -23,15 +26,27 @@ public class ResStyleValueAj {
      * package brut.androlib.res.data.value
      * public class ResStyleValue extends ResBagValue implements ResValuesXmlSerializable
      * public void serializeToResValuesXml(XmlSerializer serializer,ResResource res)
+     *
+
+     <B>com.carrot.carrotfantasy.apk</B>
+
+     Exception in thread "main" java.lang.ClassCastException: brut.androlib.res.data.value.ResStyleValue cannot be cast to brut.androlib.res.data.value.ResAttr
+     at brut.androlib.res.data.value.ResStyleValue.serializeToResValuesXml(ResStyleValue.java:65)
+     at brut.androlib.res.AndrolibResources.generateValuesFile(AndrolibResources.java:564)
+     at brut.androlib.res.AndrolibResources.decode(AndrolibResources.java:327)
+     at brut.androlib.Androlib.decodeResourcesFull(Androlib.java:123)
+     at brut.androlib.ApkDecoder.decode(ApkDecoder.java:102)
+     at brut.apktool.Main.cmdDecode(Main.java:170)
+     at brut.apktool.Main.main(Main.java:86)
+
      */
     @Around("execution(void brut.androlib.res.data.value.ResStyleValue.serializeToResValuesXml(..))" +
             "&& args(serializer, res)")
-    public void serializeToResValuesXml_around(ProceedingJoinPoint joinPoint, XmlSerializer serializer, ResResource res) {
+    public void serializeToResValuesXml(ProceedingJoinPoint joinPoint, XmlSerializer serializer,
+                                        ResResource res) throws IOException, AndrolibException {
         try {
-
             ResReferenceValue mParent = (ResReferenceValue) ReflectUtil.getFieldValue(joinPoint.getThis(), "mParent");
             Duo<ResReferenceValue, ResScalarValue>[] mItems = (Duo<ResReferenceValue, ResScalarValue>[]) ReflectUtil.getFieldValue(joinPoint.getThis(), "mItems");
-
             serializer.startTag(null, "style");
             serializer.attribute(null, "name", res.getResSpec().getName());
             if (!mParent.isNull()) {
@@ -41,19 +56,26 @@ public class ResStyleValueAj {
             }
             for (int i = 0; i < mItems.length; i++) {
                 ResResSpec spec = mItems[i].m1.getReferent();
+                String name = null;
+                String value = null;
 
+                String resource = spec.getDefaultResource().getValue().toString();
                 // hacky-fix remove bad ReferenceVars
-                if (spec.getDefaultResource().getValue().toString()
-                        .contains("ResReferenceValue@")) {
+                if (resource.contains("ResReferenceValue@")) {
                     continue;
+                } else if (resource.contains("ResStringValue@")) {
+                    name = "@" + spec.getFullName(res.getResSpec().getPackage(), false);
+                } else {
+//                    ResAttr attr = (ResAttr) spec.getDefaultResource().getValue();
+                    ResValue resValue = spec.getDefaultResource().getValue();
+                    if (!(resValue instanceof ResAttr)) {
+                        //异常跳过
+                        continue;
+                    }
+                    ResAttr attr = (ResAttr)resValue;
+                    value = attr.convertToResXmlFormat(mItems[i].m2);
+                    name = spec.getFullName(res.getResSpec().getPackage(), true);
                 }
-                ResValue resValue = spec.getDefaultResource().getValue();
-                if (!(resValue instanceof ResAttr)) {
-                    //异常跳过
-                    continue;
-                }
-                ResAttr attr = (ResAttr)resValue;
-                String value = attr.convertToResXmlFormat(mItems[i].m2);
 
                 if (value == null) {
                     value = mItems[i].m2.encodeAsResXmlValue();
@@ -64,14 +86,15 @@ public class ResStyleValueAj {
                 }
 
                 serializer.startTag(null, "item");
-                serializer.attribute(null, "name",
-                        spec.getFullName(res.getResSpec().getPackage(), true));
+                serializer.attribute(null, "name", name);
                 serializer.text(value);
                 serializer.endTag(null, "item");
             }
             serializer.endTag(null, "style");
-        } catch (Exception e) {
-            throw new ShakaRuntimeException(e);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
         }
     }
 
--- src/main/java/com/rover12421/shaka/apktool/AspjectJ/apktool_lib/StringBlockAj.java
+++ src/main/java/com/rover12421/shaka/apktool/AspjectJ/apktool_lib/StringBlockAj.java
@@ -8,16 +8,38 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * Created by rover12421 on 8/9/14.
  */
 @Aspect
 public class StringBlockAj {
-    private static final int CHUNK_TYPE = 0x001C0001;
+    // ResChunk_header = header.type (0x0001) + header.headerSize (0x001C)
+    private static final int CHUNK_STRINGPOOL_TYPE = 0x001C0001;
+    private static final int CHUNK_NULL_TYPE = 0x00000000;
     private static final int UTF8_FLAG = 0x00000100;
 
     /**
+     *
+     I: Using ShakaApktool 1.0-747a5b-2.0.0-a7b06f-SNAPSHOT
+     I: Using Apktool 2.0.0-a7b06f-SNAPSHOT on AutorunManagerv4.0.1.apk
+     I: Loading resource table...
+     I: Loading resource table...
+     I: Decoding AndroidManifest.xml with resources...
+     I: Loading resource table from file: /home/rover12421/apktool/framework/1.apk
+     I: Regular manifest package...
+     I: Decoding file-resources...
+     S: Could not decode file, replacing by FALSE value: layout/receiverrow.xml
+     S: Could not decode file, replacing by FALSE value: layout/abs__action_bar_tab_bar_view.xml
+     S: Could not decode file, replacing by FALSE value: layout/abs__action_bar_home.xml
+     S: Could not decode file, replacing by FALSE value: layout-large/abs__action_mode_close_item.xml
+     S: Could not decode file, replacing by FALSE value: layout/abs__action_menu_layout.xml
+     S: Could not decode file, replacing by FALSE value: layout/abs__action_bar_tab.xml
+     S: Could not decode file, replacing by FALSE value: layout/frg_application.xml
+     S: Could not decode file, replacing by FALSE value: layout/abs__search_view.xml
+     ...
+
      * public static StringBlock read(ExtDataInput reader) throws IOException
      */
 
@@ -26,10 +48,12 @@ public class StringBlockAj {
     public StringBlock read_around(ExtDataInput reader) {
         StringBlock block = null;
         try {
-            reader.skipCheckInt(CHUNK_TYPE);
+            reader.skipCheckChunkTypeInt(CHUNK_STRINGPOOL_TYPE, CHUNK_NULL_TYPE);
             int chunkSize = reader.readInt();
+
+            // ResStringPool_header
             int stringCount = reader.readInt();
-            int styleOffsetCount = reader.readInt();
+            int styleCount = reader.readInt();
             int flags = reader.readInt();
             int stringsOffset = reader.readInt();
             int stylesOffset = reader.readInt();
@@ -37,31 +61,24 @@ public class StringBlockAj {
 //            StringBlock block = new StringBlock();
             block = (StringBlock) ReflectUtil.newInstance(StringBlock.class);
 //            block.m_isUTF8 = (flags & UTF8_FLAG) != 0;
-//            block.m_stringOffsets = reader.readIntArray(stringCount);
-//            block.m_stringOwns = new int[stringCount];
             ReflectUtil.setFieldValue(block, "m_isUTF8", (flags & UTF8_FLAG) != 0);
+//            block.m_stringOffsets = reader.readIntArray(stringCount);
             ReflectUtil.setFieldValue(block, "m_stringOffsets", reader.readIntArray(stringCount));
+//            block.m_stringOwns = new int[stringCount];
             int[] stringOwns = new int[stringCount];
             ReflectUtil.setFieldValue(block, "m_stringOwns", stringOwns);
+//            Arrays.fill(block.m_stringOwns, -1);
+            Arrays.fill(stringOwns, -1);
 
-
-            for (int i = 0; i < stringCount; i++) {
-//                block.m_stringOwns[i] = -1;
-                stringOwns[i] = -1;
+            if (styleCount != 0) {
+//                block.m_styleOffsets = reader.readIntArray(styleCount);
+                ReflectUtil.setFieldValue(block, "m_styleOffsets", reader.readIntArray(styleCount));
             }
-            if (styleOffsetCount != 0) {
-//                block.m_styleOffsets = reader.readIntArray(styleOffsetCount);
-                ReflectUtil.setFieldValue(block, "m_styleOffsets", reader.readIntArray(styleOffsetCount));
-            } else {
-                stylesOffset = 0;
-            }
-
             {
-                int size = ((stylesOffset == 0) ? chunkSize : stylesOffset)
-                        - stringsOffset;
+//                int size = ((stylesOffset == 0) ? chunkSize : stylesOffset) - stringsOffset;
+                int size = ((styleCount == 0) ? chunkSize : stylesOffset) - stringsOffset;
                 if ((size % 4) != 0) {
-                    throw new IOException("String data size is not multiple of 4 ("
-                            + size + ").");
+                    throw new IOException("String data size is not multiple of 4 (" + size + ").");
                 }
 //                block.m_strings = new byte[size];
 //                reader.readFully(block.m_strings);
@@ -69,11 +86,11 @@ public class StringBlockAj {
                 reader.readFully(strings);
                 ReflectUtil.setFieldValue(block, "m_strings", strings);
             }
-            if (stylesOffset != 0) {
+//            if (stylesOffset != 0) {
+            if (styleCount != 0) {
                 int size = (chunkSize - stylesOffset);
                 if ((size % 4) != 0) {
-                    throw new IOException("Style data size is not multiple of 4 ("
-                            + size + ").");
+                    throw new IOException("Style data size is not multiple of 4 (" + size + ").");
                 }
 //                block.m_styles = reader.readIntArray(size / 4);
                 ReflectUtil.setFieldValue(block, "m_styles", reader.readIntArray(size / 4));
