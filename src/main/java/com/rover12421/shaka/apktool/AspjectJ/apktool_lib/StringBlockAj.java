1bf7fde02d04764631a062ffe9cc70c08a98d048
Rover12421 <rover12421@163.com>
2014-11-03 13:44:35 +0800

    update apktool
    1.update submodel apktool to ce2088df80550b181d4b78db1c16806f5146b0ee
    2.remove StringBlock AspjectJ Code. The 4-byte aligned fix in
    9b1c7d22ef...

--- src/main/java/com/rover12421/shaka/apktool/AspjectJ/apktool_lib/StringBlockAj.java
+++ src/main/java/com/rover12421/shaka/apktool/AspjectJ/apktool_lib/StringBlockAj.java
@@ -43,66 +43,71 @@ public class StringBlockAj {
      * public static StringBlock read(ExtDataInput reader) throws IOException
      */
 
-    @Around("execution(* brut.androlib.res.decoder.StringBlock.read(..))" +
-            "&& args(reader)")
-    public StringBlock read_around(ExtDataInput reader) {
-        StringBlock block = null;
-        try {
-            reader.skipCheckChunkTypeInt(CHUNK_STRINGPOOL_TYPE, CHUNK_NULL_TYPE);
-            int chunkSize = reader.readInt();
-
-            // ResStringPool_header
-            int stringCount = reader.readInt();
-            int styleCount = reader.readInt();
-            int flags = reader.readInt();
-            int stringsOffset = reader.readInt();
-            int stylesOffset = reader.readInt();
-
-//            StringBlock block = new StringBlock();
-            block = (StringBlock) ReflectUtil.newInstance(StringBlock.class);
-//            block.m_isUTF8 = (flags & UTF8_FLAG) != 0;
-            ReflectUtil.setFieldValue(block, "m_isUTF8", (flags & UTF8_FLAG) != 0);
-//            block.m_stringOffsets = reader.readIntArray(stringCount);
-            ReflectUtil.setFieldValue(block, "m_stringOffsets", reader.readIntArray(stringCount));
-//            block.m_stringOwns = new int[stringCount];
-            int[] stringOwns = new int[stringCount];
-            ReflectUtil.setFieldValue(block, "m_stringOwns", stringOwns);
-//            Arrays.fill(block.m_stringOwns, -1);
-            Arrays.fill(stringOwns, -1);
-
-            if (styleCount != 0) {
-//                block.m_styleOffsets = reader.readIntArray(styleCount);
-                ReflectUtil.setFieldValue(block, "m_styleOffsets", reader.readIntArray(styleCount));
-            }
-            {
-//                int size = ((stylesOffset == 0) ? chunkSize : stylesOffset) - stringsOffset;
-                int size = ((styleCount == 0) ? chunkSize : stylesOffset) - stringsOffset;
-                if ((size % 4) != 0) {
-                    throw new IOException("String data size is not multiple of 4 (" + size + ").");
-                }
-//                block.m_strings = new byte[size];
-//                reader.readFully(block.m_strings);
-                byte[] strings = new byte[size];
-                reader.readFully(strings);
-                ReflectUtil.setFieldValue(block, "m_strings", strings);
-            }
-//            if (stylesOffset != 0) {
-            if (styleCount != 0) {
-                int size = (chunkSize - stylesOffset);
-                if ((size % 4) != 0) {
-                    throw new IOException("Style data size is not multiple of 4 (" + size + ").");
-                }
-//                block.m_styles = reader.readIntArray(size / 4);
-                ReflectUtil.setFieldValue(block, "m_styles", reader.readIntArray(size / 4));
-            }
-
-//            return block;
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new ShakaRuntimeException(e);
-        }
-
-        return block;
-    }
+    /**
+     * fix : 9b1c7d22ef7ef14207b781164a25c892b5da54af
+     * @param reader
+     * @return
+     */
+//    @Around("execution(* brut.androlib.res.decoder.StringBlock.read(..))" +
+//            "&& args(reader)")
+//    public StringBlock read_around(ExtDataInput reader) {
+//        StringBlock block = null;
+//        try {
+//            reader.skipCheckChunkTypeInt(CHUNK_STRINGPOOL_TYPE, CHUNK_NULL_TYPE);
+//            int chunkSize = reader.readInt();
+//
+//            // ResStringPool_header
+//            int stringCount = reader.readInt();
+//            int styleCount = reader.readInt();
+//            int flags = reader.readInt();
+//            int stringsOffset = reader.readInt();
+//            int stylesOffset = reader.readInt();
+//
+////            StringBlock block = new StringBlock();
+//            block = (StringBlock) ReflectUtil.newInstance(StringBlock.class);
+////            block.m_isUTF8 = (flags & UTF8_FLAG) != 0;
+//            ReflectUtil.setFieldValue(block, "m_isUTF8", (flags & UTF8_FLAG) != 0);
+////            block.m_stringOffsets = reader.readIntArray(stringCount);
+//            ReflectUtil.setFieldValue(block, "m_stringOffsets", reader.readIntArray(stringCount));
+////            block.m_stringOwns = new int[stringCount];
+//            int[] stringOwns = new int[stringCount];
+//            ReflectUtil.setFieldValue(block, "m_stringOwns", stringOwns);
+////            Arrays.fill(block.m_stringOwns, -1);
+//            Arrays.fill(stringOwns, -1);
+//
+//            if (styleCount != 0) {
+////                block.m_styleOffsets = reader.readIntArray(styleCount);
+//                ReflectUtil.setFieldValue(block, "m_styleOffsets", reader.readIntArray(styleCount));
+//            }
+//            {
+////                int size = ((stylesOffset == 0) ? chunkSize : stylesOffset) - stringsOffset;
+//                int size = ((styleCount == 0) ? chunkSize : stylesOffset) - stringsOffset;
+//                if ((size % 4) != 0) {
+//                    throw new IOException("String data size is not multiple of 4 (" + size + ").");
+//                }
+////                block.m_strings = new byte[size];
+////                reader.readFully(block.m_strings);
+//                byte[] strings = new byte[size];
+//                reader.readFully(strings);
+//                ReflectUtil.setFieldValue(block, "m_strings", strings);
+//            }
+////            if (stylesOffset != 0) {
+//            if (styleCount != 0) {
+//                int size = (chunkSize - stylesOffset);
+//                if ((size % 4) != 0) {
+//                    throw new IOException("Style data size is not multiple of 4 (" + size + ").");
+//                }
+////                block.m_styles = reader.readIntArray(size / 4);
+//                ReflectUtil.setFieldValue(block, "m_styles", reader.readIntArray(size / 4));
+//            }
+//
+////            return block;
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            throw new ShakaRuntimeException(e);
+//        }
+//
+//        return block;
+//    }
 
 }
