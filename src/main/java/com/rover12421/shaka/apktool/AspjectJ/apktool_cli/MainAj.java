572f6f7508b9ae2bb2774f97559fa83116a03e8b
Rover12421 <rover12421@163.com>
2015-02-09 23:34:47 +0800

    添加多语言支持
    功能实现,语言文件未完成

--- src/main/java/com/rover12421/shaka/apktool/AspjectJ/apktool_cli/MainAj.java
+++ src/main/java/com/rover12421/shaka/apktool/AspjectJ/apktool_cli/MainAj.java
@@ -1,13 +1,18 @@
 package com.rover12421.shaka.apktool.AspjectJ.apktool_cli;
 
 import brut.androlib.Androlib;
+import brut.androlib.ApkDecoder;
 import brut.androlib.ApktoolProperties;
+import com.rover12421.shaka.apktool.Main;
+import com.rover12421.shaka.apktool.lib.MultiLanguageSupport;
 import com.rover12421.shaka.apktool.lib.ShakaProperties;
-import org.apache.commons.cli.CommandLine;
+import com.rover12421.shaka.apktool.util.ReflectUtil;
+import org.apache.commons.cli.*;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
+
+import java.util.Locale;
 
 /**
  * Created by rover12421 on 7/11/14.
@@ -36,5 +41,38 @@ public class MainAj {
         System.out.println("Apktool v" + Androlib.getVersion());
         System.out.println("smali v" + ApktoolProperties.get("smaliVersion"));
         System.out.println("baksmali v" + ApktoolProperties.get("baksmaliVersion"));
+    }
+
+    @Before("execution(void brut.apktool.Main._Options())")
+    public void before_Options() {
+        Option language = OptionBuilder.withLongOpt("language")
+                .withDescription("Display language, e.g. zh-CN, zh-TW")
+                .hasArg(true)
+                .withArgName("Locale")
+                .create("lng");
+
+        try {
+            Options normalOptions = (Options) ReflectUtil.getFieldValue(brut.apktool.Main.class, "normalOptions");
+            normalOptions.addOption(language);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Before("call(* org.apache.commons.cli.CommandLineParser.parse(..))" +
+            "&& args(options, arguments, stopAtNonOption)" +
+            "&& within(brut.apktool.Main)")
+    public void before_parse(Options options, String[] arguments, boolean stopAtNonOption) {
+        CommandLineParser parser = new PosixParser();
+        CommandLine commandLine;
+
+        try {
+            commandLine = parser.parse(options, arguments, stopAtNonOption);
+            if (commandLine.hasOption("lng") || commandLine.hasOption("language")) {
+                Locale locale = Locale.forLanguageTag(commandLine.getOptionValue("lng"));
+                MultiLanguageSupport.getInstance().setLang(locale);
+            }
+        } catch (Exception ex) {
+        }
     }
 }
