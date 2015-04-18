1a3476ee68438cd37e44fc263d8763767883259f
Rover12421 <rover12421@163.com>
2015-04-18 15:50:42 +0800

    Repeat Resource fix

--- src/main/java/com/rover12421/shaka/apktool/AspjectJ/apktool_cli/ResConfigAj.java
+++ src/main/java/com/rover12421/shaka/apktool/AspjectJ/apktool_cli/ResConfigAj.java
@@ -6,7 +6,6 @@ import com.rover12421.shaka.apktool.util.LogHelper;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
 
 /**
  * Created by rover12421 on 8/2/14.
@@ -16,15 +15,11 @@ public class ResConfigAj {
 
     @Around("execution(void brut.androlib.res.data.ResConfig.addResource(..))" +
             "&& args(res, overwrite)")
-    public void usage_around(ProceedingJoinPoint joinPoint, ResResource res, boolean overwrite) {
+    public void addResource(ProceedingJoinPoint joinPoint, ResResource res, boolean overwrite) throws Throwable {
         try {
-            try {
-                joinPoint.proceed(joinPoint.getArgs());
-            } catch (AndrolibException e) {
-                LogHelper.getLogger().warning("Add Repeat Resource : " + e.getMessage());
-            }
-        } catch (Throwable throwable) {
-            throwable.printStackTrace();
+            joinPoint.proceed(joinPoint.getArgs());
+        } catch (AndrolibException e) {
+            LogHelper.getLogger().warning("Add Repeat Resource > " + e.getMessage());
         }
     }
 }
