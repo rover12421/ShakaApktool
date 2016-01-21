06ece652395bc9f38c1f97f3edac4f76f16d7ce5
Rover12421 <rover12421@163.com>
2016-01-21 18:16:15 +0800

    skip `readSmalUnint`,`readOptionalUnit` Exception

--- shaka.smali/src/main/java/com/rover12421/shaka/smali/dexlib2/BaseDexBufferAj.java
+++ shaka.smali/src/main/java/com/rover12421/shaka/smali/dexlib2/BaseDexBufferAj.java
@@ -2,6 +2,7 @@ package com.rover12421.shaka.smali.dexlib2;
 
 import com.rover12421.shaka.lib.LogHelper;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.jf.dexlib2.dexbacked.BaseDexBuffer;
 
@@ -11,8 +12,8 @@ import org.jf.dexlib2.dexbacked.BaseDexBuffer;
 @Aspect
 public class BaseDexBufferAj {
 
-//    @Around("execution(* org.jf.dexlib2.dexbacked.BaseDexBuffer.readSmallUint(..))" +
-//            "&& args(offset)")
+    @Around("execution(* org.jf.dexlib2.dexbacked.BaseDexBuffer.readSmallUint(..))" +
+            "&& args(offset)")
     public int readSmallUint(ProceedingJoinPoint joinPoint, int offset) {
         BaseDexBuffer thiz = (BaseDexBuffer) joinPoint.getThis();
 
@@ -23,13 +24,14 @@ public class BaseDexBufferAj {
                 ((buf[offset+2] & 0xff) << 16) |
                 ((buf[offset+3]) << 24);
         if (result < 0) {
-            LogHelper.fine(String.format("Encountered small uint that is out of range at offset 0x%x", offset));
+            LogHelper.warning(String.format("Encountered small uint that is out of range at offset 0x%x. Set result to 0", offset));
+            result = 0;
         }
         return result;
     }
 
-//    @Around("execution(* org.jf.dexlib2.dexbacked.BaseDexBuffer.readOptionalUint(..))" +
-//            "&& args(offset)")
+    @Around("execution(* org.jf.dexlib2.dexbacked.BaseDexBuffer.readOptionalUint(..))" +
+            "&& args(offset)")
     public int readOptionalUint(ProceedingJoinPoint joinPoint, int offset) {
         BaseDexBuffer thiz = (BaseDexBuffer) joinPoint.getThis();
 
@@ -40,7 +42,7 @@ public class BaseDexBufferAj {
                 ((buf[offset+2] & 0xff) << 16) |
                 ((buf[offset+3]) << 24);
         if (result < -1) {
-            LogHelper.fine(String.format("Encountered optional uint that is out of range at offset 0x%x", offset));
+            LogHelper.warning(String.format("Encountered optional uint that is out of range at offset 0x%x. Set result to 0", offset));
         }
         return result;
     }
